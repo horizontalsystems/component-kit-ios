8bdd5491e4442c966b29f74744e2dd2513a29fa4
max <trickster77777@gmail.com>
2020-08-04 12:53:31 +0600

    Add dopdown cell with dropdown image

---
+++ ThemeKit/Classes/Cells/Views/DropdownCellView.swift
@@ -0,0 +1,24 @@
+import UIKit
+import SnapKit
+
+open class DropdownCellView: UIView {
+    private let imageView = UIImageView(image: ThemeKit.image(named: "Dropdown Indicator"))
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+
+        imageView.setContentCompressionResistancePriority(.required, for: .horizontal)
+        imageView.setContentHuggingPriority(.required, for: .horizontal)
+
+        addSubview(imageView)
+        imageView.snp.makeConstraints { maker in
+            maker.leading.centerY.equalToSuperview()
+            maker.trailing.equalToSuperview().inset(CGFloat.margin4x)
+        }
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
