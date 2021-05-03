951b0a0539062a411930ec0b11d710f4cdd5877c
max <trickster77777@gmail.com>
2021-05-03 16:28:26 +0600

    Let continue app life cycle after jailbreak detection

--- StorageKit/Classes/NoPasscode/NoPasscodeViewController.swift
+++ StorageKit/Classes/NoPasscode/NoPasscodeViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import SnapKit
 import ThemeKit
+import ComponentKit
 
 public class NoPasscodeViewController: ThemeViewController {
     public enum Mode {
@@ -10,12 +11,16 @@ public class NoPasscodeViewController: ThemeViewController {
 
     private let mode: Mode
 
+    private let containerView = UIView()
     private let wrapperView = UIView()
     private let iconImageView = UIImageView()
     private let infoLabel = UILabel()
 
-    public init(mode: Mode) {
+    private let completion: (() -> ())?
+
+    public init(mode: Mode, completion: (() -> ())? = nil) {
         self.mode = mode
+        self.completion = completion
 
         super.init()
     }
@@ -27,28 +32,31 @@ public class NoPasscodeViewController: ThemeViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(wrapperView)
-        wrapperView.addSubview(iconImageView)
-        wrapperView.addSubview(infoLabel)
+        view.addSubview(containerView)
+        containerView.snp.makeConstraints { maker in
+            maker.leading.top.trailing.equalToSuperview()
+        }
 
+        containerView.addSubview(wrapperView)
         wrapperView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.centerY.equalToSuperview()
         }
 
+        wrapperView.addSubview(iconImageView)
         iconImageView.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.centerX.equalToSuperview()
-            make.size.equalTo(72)
         }
 
         iconImageView.contentMode = .center
         iconImageView.image = StorageKit.image(named: "attention_48")?.withRenderingMode(.alwaysTemplate)
         iconImageView.tintColor = .themeGray
 
+        wrapperView.addSubview(infoLabel)
         infoLabel.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(42)
-            make.top.equalTo(self.iconImageView.snp.bottom).offset(CGFloat.margin8x)
+            make.top.equalTo(iconImageView.snp.bottom).offset(CGFloat.margin8x)
             make.bottom.equalToSuperview()
         }
 
@@ -56,12 +64,35 @@ public class NoPasscodeViewController: ThemeViewController {
         infoLabel.textAlignment = .center
         infoLabel.numberOfLines = 0
         infoLabel.font = .body
+
         switch mode {
         case .jailbreak:
             infoLabel.text = "jailbreak.info_text".localized
+
+            let understandButton = ThemeButton()
+            view.addSubview(understandButton)
+            understandButton.snp.makeConstraints { maker in
+                maker.centerX.equalToSuperview()
+                maker.leading.trailing.equalToSuperview().inset(CGFloat.margin24)
+                maker.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(CGFloat.margin32)
+                maker.top.equalTo(containerView.snp.bottom).offset(CGFloat.margin32)
+            }
+
+            understandButton.apply(style: .primaryYellow)
+            understandButton.setTitle("button.i_understand".localized, for: .normal)
+            understandButton.addTarget(self, action: #selector(onUnderstand), for: .touchUpInside)
         case .noPasscode:
             infoLabel.text = "no_passcode.info_text".localized
+
+            containerView.snp.makeConstraints { maker in
+                maker.bottom.equalToSuperview()
+            }
         }
+
+    }
+
+    @objc private func onUnderstand() {
+        dismiss(animated: true, completion: completion)
     }
 
 }
