8bb26df74cdde341b030f9e12beb9ba1a2b40839
max <trickster77777@gmail.com>
2021-11-03 16:22:45 +0600

    Calculate filter cell width to fit badge

--- ComponentKit/Classes/Cells/Views/BadgeView.swift
+++ ComponentKit/Classes/Cells/Views/BadgeView.swift
@@ -2,13 +2,17 @@ import UIKit
 import SnapKit
 
 public class BadgeView: UIView {
+    static private let sideMargin: CGFloat = .margin1x
+    static private let height: CGFloat = 15
+    static private let font: UIFont = .microSB
+
     private let label = UILabel()
 
     override public init(frame: CGRect) {
         super.init(frame: frame)
 
         snp.makeConstraints { maker in
-            maker.height.equalTo(15)
+            maker.height.equalTo(Self.height)
         }
 
         layer.cornerRadius = .cornerRadius1x
@@ -16,14 +20,14 @@ public class BadgeView: UIView {
 
         addSubview(label)
         label.snp.makeConstraints { maker in
-            maker.leading.trailing.equalToSuperview().inset(CGFloat.margin1x)
+            maker.leading.trailing.equalToSuperview().inset(Self.sideMargin)
             maker.centerY.equalToSuperview()
         }
 
         label.setContentCompressionResistancePriority(.required, for: .horizontal)
         label.setContentHuggingPriority(.required, for: .horizontal)
         label.textColor = .themeBran
-        label.font = .microSB
+        label.font = Self.font
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -49,6 +53,10 @@ public class BadgeView: UIView {
     public var text: String? {
         get { label.text }
         set { label.text = newValue }
+    }
+
+    static public func width(for text: String) -> CGFloat {
+        text.size(containerWidth: .greatestFiniteMagnitude, font: font).width + sideMargin * 2
     }
 
 }
